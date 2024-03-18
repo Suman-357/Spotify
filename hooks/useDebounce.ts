39591369a68e100d0ba5551fbea85c662ace51db
Suman-357 <4mh20cs098suman@gmail.com>
2024-03-18 22:09:40 +0530

    search functionality

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+
+function useDebounce<T>(value: T, delay?: number): T {
+    const [debouncedvalue, setdebouncedvalue] = useState<T>(value);
+
+    useEffect(()=>{
+        const timer =setTimeout(()=>{
+            setdebouncedvalue(value)
+        }, delay || 500);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [value, delay]);
+
+    return debouncedvalue;
+};
+
+export default useDebounce;
