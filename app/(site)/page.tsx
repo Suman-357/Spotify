dc9991d758eb7e569adc3eeee473d79b013277cb
Suman-357 <4mh20cs098suman@gmail.com>
2024-03-19 10:51:23 +0530

    Liked songs page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <Listitems image="/images/Liked.png" name="Liked Songs" href="Liked"/>
+            <Listitems image="/images/Liked.png" name="Liked Songs" href="liked"/>
         </div>
       </Header>
       <div className="mt-2 mb-7 px-6">
